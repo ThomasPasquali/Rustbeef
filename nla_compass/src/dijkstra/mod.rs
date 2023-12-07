8f5fa98554a1269c0abc73522219d83d68e55c42
sasso0101 <salvatore.andaloro01@gmail.com>
2023-12-07 01:59:22 +0100

    Rewritten successors for dijkstra library

--- nla_compass/src/dijkstra/mod.rs
+++ nla_compass/src/dijkstra/mod.rs
@@ -1,10 +1,40 @@
+use std::collections::HashMap;
+use std::hash::Hasher;
 use std::ops::Div;
 use pathfinding::prelude::{build_path, dijkstra_all};
 use robotics_lib::interface::Direction;
 use robotics_lib::interface::robot_map;
+use robotics_lib::world::coordinates::Coordinate;
 use robotics_lib::world::tile::Tile;
 use robotics_lib::world::World;
 use endless_heights::MAP_SIZE;
+use core::hash::Hash;
+
+struct Wrapper<'a> {
+    world: &'a Vec<Vec<Option<Tile>>>,
+    row: usize,
+    col: usize
+}
+impl<'a> PartialEq for Wrapper<'a> {
+    fn eq(&self, other: &Self) -> bool {
+        self.row == other.row && self.col == other.col
+    }
+}
+impl<'a> Eq for Wrapper<'a> {}
+impl<'a> Hash for Wrapper<'a> {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        (self.row, self.col).hash(state);
+    }
+}
+impl<'a> Clone for Wrapper<'a> {
+    fn clone(&self) -> Self {
+        Self {
+            world: self.world,
+            row: self.row,
+            col: self.col
+        }
+    }
+}
 
 pub fn dijkstra_path(start: &(u32, u32), destination: &(u32, u32), mut world: &World) -> Vec<Direction>{
     let robot_world=robot_map(&mut world).unwrap();
@@ -30,7 +60,22 @@ pub fn dijkstra_path(start: &(u32, u32), destination: &(u32, u32), mut world: &W
         }
         unknown_tiles
     }
-    fn successors(&n: &u32) -> Vec<(u32, usize)> {
+    // TODO: THIS FUNCTION HAS NOT BEEN TESTED!!!
+    fn successors<'a>(&node: &'a Wrapper) -> Vec<(Wrapper<'a>, i32)>
+    {
+        let mut successors = Vec::new();
+        // Iterate over 3x3 adjacent tiles
+        for row in [node.row-1, node.row+1] {
+            for col in [node.col-1, node.col+1] {
+                if let Some(_) = node.world[row][col] {
+                    successors.push((Wrapper {
+                        world: node.world, row, col
+                    }, 10)) //TODO: Swap 10 with actual cost
+                }
+            };
+        }
+        successors
+        /*
         let price = 10;
         // let locked = get_locked(robot_world);
         let locked = vec![];
@@ -136,14 +181,24 @@ pub fn dijkstra_path(start: &(u32, u32), destination: &(u32, u32), mut world: &W
                     }
                 }
             } else { vec![] }
-        } else { vec![] }
-    };
+        } else { vec![] }*/
+    }
     let start_n = convert_to_number(*start);
     let destination_n = convert_to_number(*destination);
-    let reachables_from_start = dijkstra_all(&start_n, successors);
-    let path = build_path(&destination_n, &reachables_from_start);
+
+    // Rewritten to take the wrapper as input
+    let reachables_from_start = dijkstra_all(
+        &Wrapper { world: &robot_world, row: start.0 as usize, col: start.1 as usize }, 
+        successors
+    );
+
+    let path = build_path(
+        &Wrapper { world: &robot_world, row: destination.0 as usize, col: destination.1 as usize }, 
+        &reachables_from_start
+    );
     let direction_list = convert_to_directions(path);
     return direction_list;
+    
 }
 
 fn bigger_multiple(n: u32) -> u32 {
@@ -164,9 +219,20 @@ fn convert_to_number(cell: (u32, u32)) -> u32 {
     (cell.0 - 1) * (MAP_SIZE as u32) + cell.1
 }
 
-fn convert_to_directions(cells: Vec<u32>) -> Vec<Direction> {
+fn convert_to_directions<'a>(cells: Vec<Wrapper<'a>>) -> Vec<Direction> {
     let mut commands: Vec<Direction> = Vec::new();
-    for i in 1..cells.len() {
+    for step in cells.windows(2) {
+        // TODO: Check that this mapping to directions is right
+        if step[1].row > step[0].row {
+            commands.push(Direction::Right);
+        } else if step[1].row < step[0].row {
+            commands.push(Direction::Left);
+        } else if step[1].col > step[0].col {
+            commands.push(Direction::Up);
+        } else if step[1].col > step[0].col {
+            commands.push(Direction::Down);
+        }
+        /*
         if cells.get(i) > cells.get(i-1) {
             if bigger_multiple(*cells.get(i).unwrap()) == bigger_multiple(*cells.get(i-1).unwrap()) {
                 commands.push(Direction::Right);
@@ -179,6 +245,7 @@ fn convert_to_directions(cells: Vec<u32>) -> Vec<Direction> {
             }
             else { commands.push(Direction::Up); }
         }
+        */
     }
     commands
 }
