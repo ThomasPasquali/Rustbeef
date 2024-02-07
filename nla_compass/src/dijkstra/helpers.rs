9d2feaa2558dc498505a0d476f1eca8e5deffd10
claudio <claudioforoncelli@gmail.com>
2024-02-07 22:04:54 +0100

    fixed dijkstra negative usize error

--- nla_compass/src/dijkstra/helpers.rs
+++ nla_compass/src/dijkstra/helpers.rs
@@ -5,10 +5,11 @@ use super::Wrapper;
 
 pub(crate) fn get_cost(current_tile: &Tile, next_tile: &Tile) -> usize {
     let mut cost: usize = next_tile.tile_type.properties().cost();
-    let elevation_diff = next_tile.elevation - current_tile.elevation;
-    if elevation_diff > 0 {
+    if next_tile.elevation > current_tile.elevation {
+        let elevation_diff = next_tile.elevation - current_tile.elevation;
         cost += elevation_diff.pow(2);
     }
+
     cost
 }
 
--- nla_compass/src/dijkstra/mod.rs
+++ nla_compass/src/dijkstra/mod.rs
@@ -8,6 +8,7 @@ use robotics_lib::interface::Direction;
 use robotics_lib::world::tile::Tile;
 use crate::compass::MoveError;
 use crate::dijkstra::helpers::get_direction;
+use std::cmp::max;
 
 pub(crate) struct Wrapper {
     pub(crate) world: Rc<Vec<Vec<Option<Tile>>>>,
@@ -39,8 +40,9 @@ impl Clone for Wrapper {
 fn successors(node: &Wrapper) -> Vec<(Wrapper, usize)> {
     let mut successors = Vec::new();
     // Iterate over 3x3 adjacent tiles
-    for row in [node.row-1, node.row+1] {
-        for col in [node.col-1, node.col+1] {
+
+    for row in [max(node.row-1, 0), node.row+1] {
+        for col in [max(node.col-1, 0), node.col+1] {
             if let Some(_) = node.world.as_ref()[row][col] {
                 successors.push((Wrapper {
                     world: node.world.clone(), row, col
